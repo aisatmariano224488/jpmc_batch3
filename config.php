ff36081387d5be57014166a4d5917d100e13caf7
aisatmariano224488 <aisat.mariano224488@gmail.com>
2025-11-13 16:10:44 +0800

    Updated Config

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 // DO NOT commit this file to version control!
 
 // Brevo API Configuration
-define('BREVO_API_KEY', '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');
-define('BREVO_SENDER_EMAIL', 'aisat.castillo222436@gmail.com');
+define('BREVO_API_KEY', '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');
+define('BREVO_SENDER_EMAIL', 'jamespolymers.international@gmail.com');
 define('BREVO_SENDER_NAME', 'JPMC Website');
 
 // Database Configuration (if not already in db_connection.php)
